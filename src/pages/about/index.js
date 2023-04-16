906f2f6e3e3ad4312e3a31a0d9e38af80cffb6c0
fernandogprieto <prieto.fernandog@gmail.com>
2023-04-16 00:11:52 -0400

    new icons .svg

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -19,12 +19,75 @@ function AboutpageHeader() {
             <p>I'm open source and cloud-native enthusiast. I enjoy discovering new technologies, contributing to community projects,
               and sharing my expertise with others in order to foster innovation and cooperation in the IT sector.</p>
               <div className="svg_row" >
-              <a href="https://gitlab.com/fernandogprieto/fgp-website" target="_blank" rel="noopener noreferrer">
+              <a href="mailto:prieto.fernandog@gmail.com" target="_blank" rel="noopener noreferrer">
+                <ThemedImage
+                  alt="Email icon"
+                  sources={{
+                  light: 'https://gitlab.com/fernandogprieto/fgp-website/-/raw/main/static/svg/spotify.svg',
+                  dark: 'https://gitlab.com/fernandogprieto/fgp-website/-/raw/main/static/svg/spotify-light.svg',
+                  }}
+                />
+              </a>
+              <a href="https://open.spotify.com/user/3132r43u32abukk6v3gwbbm64vx4?si=a0b6dbdbab8b4688&nd=1" target="_blank" rel="noopener noreferrer">
+                <ThemedImage
+                  alt="Spotify icon"
+                  sources={{
+                  light: 'https://gitlab.com/fernandogprieto/fgp-website/-/raw/main/static/svg/spotify.svg',
+                  dark: 'https://gitlab.com/fernandogprieto/fgp-website/-/raw/main/static/svg/spotify-light.svg',
+                  }}
+                />
+              </a>
+              <a href="https://discord.gg/zAZC37Mfxp" target="_blank" rel="noopener noreferrer">
                 <ThemedImage
                   alt="Discord icon"
                   sources={{
-                  light: 'https://gitlab.com/fernandogprieto/fgp-website/-/raw/main/static/svg/discord.svg',
-                  dark: 'https://gitlab.com/fernandogprieto/fgp-website/-/raw/main/static/svg/discord-light.svg',
+                  light: 'https://gitlab.com/fernandogprieto/fgp-website/-/raw/main/static/svg/spotify.svg',
+                  dark: 'https://gitlab.com/fernandogprieto/fgp-website/-/raw/main/static/svg/spotify-light.svg',
+                  }}
+                />
+              </a>
+              <a href="https://twitter.com/fernandogprieto" target="_blank" rel="noopener noreferrer">
+                <ThemedImage
+                  alt="Twitter icon"
+                  sources={{
+                  light: 'https://gitlab.com/fernandogprieto/fgp-website/-/raw/main/static/svg/spotify.svg',
+                  dark: 'https://gitlab.com/fernandogprieto/fgp-website/-/raw/main/static/svg/spotify-light.svg',
+                  }}
+                />
+              </a>
+              <a href="https://www.linkedin.com/in/fernando-prieto-carabobo/" target="_blank" rel="noopener noreferrer">
+                <ThemedImage
+                  alt="LinkedIn icon"
+                  sources={{
+                  light: 'https://gitlab.com/fernandogprieto/fgp-website/-/raw/main/static/svg/spotify.svg',
+                  dark: 'https://gitlab.com/fernandogprieto/fgp-website/-/raw/main/static/svg/spotify-light.svg',
+                  }}
+                />
+              </a>
+              <a href="https://www.credly.com/users/fernandoprieto/badges" target="_blank" rel="noopener noreferrer">
+                <ThemedImage
+                  alt="Creedly icon"
+                  sources={{
+                  light: 'https://gitlab.com/fernandogprieto/fgp-website/-/raw/main/static/svg/spotify.svg',
+                  dark: 'https://gitlab.com/fernandogprieto/fgp-website/-/raw/main/static/svg/spotify-light.svg',
+                  }}
+                />
+              </a>
+              <a href="https://dev.to/fernandogprieto" target="_blank" rel="noopener noreferrer">
+                <ThemedImage
+                  alt="Dev Community icon"
+                  sources={{
+                  light: 'https://gitlab.com/fernandogprieto/fgp-website/-/raw/main/static/svg/spotify.svg',
+                  dark: 'https://gitlab.com/fernandogprieto/fgp-website/-/raw/main/static/svg/spotify-light.svg',
+                  }}
+                />
+              </a>
+              <a href="https://g.dev/fernandogprieto" target="_blank" rel="noopener noreferrer">
+                <ThemedImage
+                  alt="Google developer icon"
+                  sources={{
+                  light: 'https://gitlab.com/fernandogprieto/fgp-website/-/raw/main/static/svg/spotify.svg',
+                  dark: 'https://gitlab.com/fernandogprieto/fgp-website/-/raw/main/static/svg/spotify-light.svg',
                   }}
                 />
               </a>
